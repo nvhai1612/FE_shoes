1dd2e5cf21ee4e74f6c03fce2b1a2f875afbbf9f
DongThanhNguyen <dongtuber@gmail.com>
2024-11-11 08:34:14 +0700

    update

---
+++ A.java
@@ -0,0 +1,31 @@
+interactive rebase in progress; onto e7d1efe
+Last command done (1 command done):
+   pick 5e3d29b them sua xoa degiay,thuonghieu,chatlieu,mausac,kichco
+Next command to do (1 remaining command):
+   pick 520ba89 them sua xoa degiay, thuonghieu, chatlieu, mausac, kichco
+  (use "git rebase --edit-todo" to view and edit)
+You are currently rebasing branch 'dongnt' on 'e7d1efe'.
+  (fix conflicts and then run "git rebase --continue")
+  (use "git rebase --skip" to skip this patch)
+  (use "git rebase --abort" to check out the original branch)
+
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        modified:   package-lock.json
+        modified:   package.json
+
+Unmerged paths:
+  (use "git restore --staged <file>..." to unstage)
+  (use "git add <file>..." to mark resolution)
+        both added:      src/pages/product/BrandList.js
+        both added:      src/pages/product/ColorList.js
+        both added:      src/pages/product/MaterialList.js
+        both added:      src/pages/product/SizeList.js
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        .idea/
+
+PS Z:\End\OK2\FE_shoes> git add .
+warning: in the working copy of '.idea/workspace.xml', LF will be replaced by CRLF the next time Git touches it
+PS Z:\End\OK2\FE_shoes>
